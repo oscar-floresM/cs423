e04241b8ec83dbae1a573870a99d16d4c51f5fc1
oscar-floresM <114531494+oscar-floresM@users.noreply.github.com>
2025-04-20 19:32:52 -0700

    Update library.py

--- library.py
+++ library.py
@@ -277,6 +277,7 @@ titanic_transformer = Pipeline(steps=[
     ('class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
     #add your new ohe step below
     ('joined', CustomOHETransformer(target_column='Joined')),
+    ('fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
     ], verbose=True)
 
 #now invoke it
