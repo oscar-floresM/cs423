ac4a3e3872b09c10850d2c9dfeb01a5988b956fe
oscar-floresM <114531494+oscar-floresM@users.noreply.github.com>
2025-05-17 16:24:12 -0700

    Update library.py

--- library.py
+++ library.py
@@ -764,3 +764,35 @@ def customer_setup(customer_table, transformer=customer_transformer, rs=customer
   return dataset_setup(customer_table, 'Rating', transformer, rs, ts)
 
 ###########################################################################################################################
+def halving_search(model, grid, x_train, y_train, factor=2, min_resources="exhaust", scoring='roc_auc'):
+  #your code below
+  halving_cv = HalvingGridSearchCV(
+      model, grid,
+      scoring = scoring,
+      n_jobs = -1,
+      min_resources = min_resources,
+      factor = factor,
+      cv = 5,
+      random_state = 1234,
+      refit = True
+  )
+
+  grid_result = halving_cv.fit(x_train, y_train)
+  return grid_result
+
+###########################################################################################################################
+def sort_grid(grid):
+  sorted_grid = grid.copy()
+
+  #sort values - note that this will expand range for you
+  for k,v in sorted_grid.items():
+    sorted_grid[k] = sorted(sorted_grid[k], key=lambda x: (x is None, x))  #handles cases where None is an alternative value
+
+  #sort keys
+  sorted_grid = dict(sorted(sorted_grid.items()))
+
+  return sorted_grid
+    
+###########################################################################################################################
+
+###########################################################################################################################
